55749985f6ce590973daf398afd12630b16cda04
DESKTOP-AGJBEEJ\Alex <alexthombaez@gmail.com>
2022-02-17 15:04:46 -0400

    Print Mangu agregado

--- Ejemplo01/Ejemplo01/Program.cs
+++ Ejemplo01/Ejemplo01/Program.cs
@@ -5,6 +5,7 @@ class Ejemplo01
     static void Main()
     {
         Console.WriteLine("Hola mundo, me llamo Alex");
+        Console.WriteLine("Mangu");
         Console.WriteLine("Por ahora no parece dificil");
     }
 }
