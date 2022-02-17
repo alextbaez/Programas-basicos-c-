34185e343bf57081edeac94619f4d18c67d74be9
alextbaez <83987802+alextbaez@users.noreply.github.com>
2022-02-17 15:05:18 -0400

    Update Program.cs
    Eliminacion de print mangu

--- Ejemplo01/Ejemplo01/Program.cs
+++ Ejemplo01/Ejemplo01/Program.cs
@@ -5,7 +5,6 @@ class Ejemplo01
     static void Main()
     {
         Console.WriteLine("Hola mundo, me llamo Alex");
-        Console.WriteLine("Mangu");
         Console.WriteLine("Por ahora no parece dificil");
     }
 }
