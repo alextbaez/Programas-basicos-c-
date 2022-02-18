912c183fc5dc61ec9188dd1ad887173c89784be9
alextbaez <83987802+alextbaez@users.noreply.github.com>
2022-02-17 21:45:26 -0400

    Optimizacion de codigo

--- Ejemplo01/Ejemplo01/Program.cs
+++ Ejemplo01/Ejemplo01/Program.cs
@@ -6,15 +6,11 @@ class Ejemplo01
     {
         Console.WriteLine("Hola mundo, me llamo Alex");
         Console.WriteLine("Por ahora no parece dificil");
-
-
         Console.WriteLine("Otro texto");
 
         Saludo();
     }
-
-
-
+    
     static void Saludo()
     {
         Console.WriteLine("Saludo");
